278a062917c4edece5515b10f7e2395f1c120e3f
huynhducmink <cnhhuynhmink@gmail.com>
2022-05-23 00:44:51 +0700

    fix for different output format of VIO packages, add RVIZ launch file

--- README.md
+++ README.md
@@ -1,17 +1,39 @@
 # VINSMONO_groundtruth_visualization
 
-This is a ROS package use to transform output of VIO (VINS_MONO, ROVIO, ORB_SLAM_3) to the world frame and visualize it in RVIZ
+This is a ROS package use to transform output of VIO (VINS_MONO, ROVIO, ORB_SLAM_3) to the world frame and visualize it in RVIZ if needed
 
-Clone the repository into catkin_ws/src and change package name to read_topic
+Clone the repository into catkin_ws/src and change package name to "read_topic"
 
-
-To run:
-```php
+## To run the transform node:
+```
 rosrun read_topic vio_transform
 ```
 
-Then choose the appropiate dataset type using input 1,2,3 or 4 then Enter. The same with VIO program type.
+Then choose the appropiate dataset type using input 1,2,3 or 4 then press Enter.
 
-Run this package before running the dataset
+## If you need to visualize in RVIZ: (VINSMONO have RVIZ built in)
+```
+roslaunch read_topic vio_transform_rviz.launch
+```
 
-Work in progress: real time running with simulation program
+The red line is the ground truth path in the world frame
+
+The white line is the VIO path in the world frame
+
+## Workflow when working with IMU+Camera dataset:
+
+- Run the VIO package
+- Run this package (and RVIZ if needed) (as VINSMONO have RVIZ built in)
+- Run the dataset
+
+## Workflow when working with live data from simulation program:
+
+- Run the VIO package
+- Run this package (and RVIZ if needed) (as VINSMONO have RVIZ built in)
+- Run the simulation program
+
+## Workflow when working with drone: (currently not support init when drone is moving, please start when drone is stationary)
+
+- Run the VIO package
+- Run this package
+- Move the drone
--- src/vio_transform.cpp
+++ src/vio_transform.cpp
@@ -4,6 +4,7 @@
 #include "nav_msgs/Odometry.h"
 #include "nav_msgs/Path.h"
 #include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include "tf/transform_broadcaster.h"
 #include <sstream>
 #include <eigen3/Eigen/Dense>
@@ -29,7 +30,7 @@ Eigen::Quaternionf q;
 Eigen::Matrix3f R;
 
 // Run 1 time to take the first message of the ground truth odometry to construct the transformation matrix
-// In case of running in real time, set this value to true then modify the world_to_body matrix in the main function to the correct matrix
+// In case of running in real time, set this value to false then modify the world_to_body matrix in the main function to the correct matrix
 // Work in progress, move this setting to a launch file
 bool init_transformation_matrix = true;
 
@@ -63,9 +64,45 @@ void ground_truth_sub_callback(const nav_msgs::Odometry msg)
   }
 }
 
-void vio_odo_sub_callback(const nav_msgs::Odometry msg)
+void vins_vio_odo_sub_callback(const nav_msgs::Odometry msg)
 {
   vio_output << msg.pose.pose.position.x,msg.pose.pose.position.y,msg.pose.pose.position.z,1;
+  vio_output_world = world_to_body*vio_output; // Transform from VIO frame to world frame
+
+	vio_odo_msg.pose.pose.position.x = vio_output_world(0,0);
+	vio_odo_msg.pose.pose.position.y = vio_output_world(1,0);
+	vio_odo_msg.pose.pose.position.z = vio_output_world(2,0);
+
+	vio_posestamped_msg.pose.position.x = vio_odo_msg.pose.pose.position.x;
+	vio_posestamped_msg.pose.position.y = vio_odo_msg.pose.pose.position.y;
+	vio_posestamped_msg.pose.position.z = vio_odo_msg.pose.pose.position.z;
+	
+	vio_path_msg.poses.push_back(vio_posestamped_msg);
+  vio_odo_msg.header.frame_id = "world";
+	vio_path_msg.header.frame_id = "world";
+}
+
+void rovio_vio_odo_sub_callback(const geometry_msgs::PoseWithCovarianceStamped msg)
+{
+  vio_output << msg.pose.pose.position.x,msg.pose.pose.position.y,msg.pose.pose.position.z,1;
+  vio_output_world = world_to_body*vio_output; // Transform from VIO frame to world frame
+
+	vio_odo_msg.pose.pose.position.x = vio_output_world(0,0);
+	vio_odo_msg.pose.pose.position.y = vio_output_world(1,0);
+	vio_odo_msg.pose.pose.position.z = vio_output_world(2,0);
+
+	vio_posestamped_msg.pose.position.x = vio_odo_msg.pose.pose.position.x;
+	vio_posestamped_msg.pose.position.y = vio_odo_msg.pose.pose.position.y;
+	vio_posestamped_msg.pose.position.z = vio_odo_msg.pose.pose.position.z;
+	
+	vio_path_msg.poses.push_back(vio_posestamped_msg);
+  vio_odo_msg.header.frame_id = "world";
+	vio_path_msg.header.frame_id = "world";
+}
+
+void orbslam3_vio_odo_sub_callback(const geometry_msgs::PoseStamped msg)
+{
+  vio_output << msg.pose.position.x,msg.pose.position.y,msg.pose.position.z,1;
   vio_output_world = world_to_body*vio_output; // Transform from VIO frame to world frame
 
 	vio_odo_msg.pose.pose.position.x = vio_output_world(0,0);
@@ -87,15 +124,21 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  int groundtruth_choice,vio_choice;
+	// Default transformation matrix
+	// Modify if necessary
+  world_to_body << 1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1;
+
+  int groundtruth_choice;
   ROS_INFO("Please input groundtruth topic");
   ROS_INFO("Input 1 for flightgoogle");
-  ROS_INFO("Input 2 for euroc");
+  ROS_INFO("Input 2 for euroc (Not working yet as groundtruth of EUROC is in different format)");
   ROS_INFO("Input 3 for airsim");
   ROS_INFO("Input 4 for flightmare");
 
   std::string groundtruth_topic;
-  std::string vio_output_topic;
 
   std::cin >> groundtruth_choice;
   if (groundtruth_choice == 1){
@@ -104,7 +147,7 @@ int main(int argc, char **argv)
   }
   else if (groundtruth_choice == 2){
     groundtruth_topic = "/leica/position";
-    ROS_INFO("Transforming output of euroc dataset");
+    ROS_INFO("Transforming output of euroc dataset (Not working yet as groundtruth of EUROC is in different format)");
   }
   else if (groundtruth_choice == 3){
     groundtruth_topic = "/mavros/local_position/odom";
@@ -119,35 +162,6 @@ int main(int argc, char **argv)
     ROS_INFO("Transforming output of flightgoogles dataset");
   }
 
-  ROS_INFO("Please input vio output topic");
-  ROS_INFO("Input 1 for vinsmono");
-  ROS_INFO("Input 2 for rovio");
-  ROS_INFO("Input 3 for orb slam 3 ");
-
-  std::cin >> vio_choice;
-  if (vio_choice == 1){
-    vio_output_topic = "/vins_estimator/odometry";
-    ROS_INFO("Using VINSMONO");
-  }
-  else if (vio_choice == 2){
-    vio_output_topic = "/rovio/pose_with_covariance_stamped";
-    ROS_INFO("Using ROVIO");
-  }
-  else if (vio_choice == 3){
-    vio_output_topic = "/out_odom";
-    ROS_INFO("Using ORB SLAM 3");
-  }
-  else{
-    vio_output_topic = "/vins_estimator/odometry";
-    ROS_INFO("Using VINSMONO");
-  }
-
-	// Default transformation matrix
-	// Modify if necessary
-  world_to_body << 1, 0, 0, 0,
-      0, 1, 0, 0,
-      0, 0, 1, 0,
-      0, 0, 0, 1;
 
   ros::Publisher ground_truth_pub_odo = n.advertise<nav_msgs::Odometry>("/ground_truth_odo", 1);
   ros::Publisher ground_truth_pub_path = n.advertise<nav_msgs::Path>("/ground_truth_path", 1);
@@ -155,7 +169,9 @@ int main(int argc, char **argv)
 
   ros::Publisher vio_pub_odo = n.advertise<nav_msgs::Odometry>("/vio_odo", 1);
   ros::Publisher vio_pub_path = n.advertise<nav_msgs::Path>("/vio_path", 1);
-	ros::Subscriber vio_odo_sub = n.subscribe(vio_output_topic,1,vio_odo_sub_callback);
+	ros::Subscriber vins_vio_odo_sub = n.subscribe("/vins_estimator/odometry",1,vins_vio_odo_sub_callback);
+	ros::Subscriber rovio_vio_odo_sub = n.subscribe("/rovio/pose_with_covariance_stamped",1,rovio_vio_odo_sub_callback);
+	ros::Subscriber orbslam3_vio_odo_sub = n.subscribe("/out_odom",1,orbslam3_vio_odo_sub_callback);
 
   ros::Rate loop_rate(10);
 
