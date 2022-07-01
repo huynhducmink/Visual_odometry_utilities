a05c8d8383318d1b2c148623bdff154f48e2690f
huynhducmink <cnhhuynhmink@gmail.com>
2022-07-02 03:38:00 +0700

    rewrite everything using proper tf transform

---
+++ include/vio_transform.h
@@ -0,0 +1,75 @@
+#include "ros/ros.h"
+#include "nav_msgs/Odometry.h"
+#include "nav_msgs/Path.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
+#include "tf/transform_broadcaster.h"
+#include "tf/transform_listener.h"
+#include <sstream>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Geometry>
+#include <string>
+#include <iostream>
+
+class VIO_transform
+{
+public:
+    VIO_transform(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private);
+    ~VIO_transform();
+
+private:
+    ros::NodeHandle nh_;
+    ros::NodeHandle nh_private_;
+
+    ros::Publisher ground_truth_pub_odo;
+    ros::Publisher ground_truth_pub_path;
+    ros::Subscriber groud_truth_sub;
+    ros::Subscriber groud_truth_sub_temp;
+
+    ros::Publisher vio_pub_odo_posestamped;
+    ros::Publisher vio_pub_path;
+    ros::Subscriber vins_vio_odo_sub;
+    ros::Subscriber rovio_vio_odo_sub;
+    ros::Subscriber orbslam3_vio_odo_sub;
+
+    ros::Rate loop_rate = 10;
+
+    std::string groundtruth_topic = "/default1";      // default
+    std::string groundtruth_topic_temp = "/default2"; // default
+
+    // Ground truth messages (in world frame)
+    nav_msgs::Odometry gt_odo_msg;
+    geometry_msgs::PoseStamped gt_posestamped_msg;
+    nav_msgs::Path gt_path_msg;
+
+    // Visual odometry messages
+    // In vio frame (in the first camera image frame)
+    nav_msgs::Odometry vio_odo_msg;
+    geometry_msgs::PoseStamped vio_posestamped_msg;
+    nav_msgs::Path vio_path_msg;
+    // In world frame
+    nav_msgs::Odometry world_vio_odo_msg;
+    geometry_msgs::PoseStamped world_vio_posestamped_msg;
+    nav_msgs::Path world_vio_path_msg;
+
+    // Use to transform from VIO frame to world frame
+    tf::Quaternion world_to_body_rotation;
+    tf::Vector3 world_to_body_origin;
+    tf::TransformBroadcaster broadcaster_transform;
+    tf::Transform br_transform;
+    tf::TransformListener listener_transform;
+    tf::StampedTransform ln_transform;
+
+    // Run 1 time to take the first message of the ground truth odometry to construct the transformation matrix
+    bool init_transformation = true;
+
+    void groundtruth_type_choice();
+    void init_pub_sub();
+    void transforming_VIO_output();
+    void sendTransform(tf::Vector3 input_origin, tf::Quaternion input_rotation);
+    void ground_truth_sub_callback(const nav_msgs::Odometry msg);
+    void ground_truth_sub_callback_temp(const geometry_msgs::PoseWithCovarianceStamped msg);
+    void vins_vio_odo_sub_callback(const nav_msgs::Odometry msg);
+    void rovio_vio_odo_sub_callback(const geometry_msgs::PoseWithCovarianceStamped msg);
+    void orbslam3_vio_odo_sub_callback(const geometry_msgs::PoseStamped msg);
+};
